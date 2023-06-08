9d6a12f5a968b6111cdc6746d9e9524ac90be852
Fabiano Mesquita Silva <fabianomsquita@gmail.com>
2023-06-08 17:19:16 -0300

    ajustes finais em valor das variáveis

--- ator.js
+++ ator.js
@@ -1,32 +1,32 @@
 //variáveis ator
-let xAtor = 100;
+let xAtor = 270;
 let yAtor = 368;
 let colisao = false;
 let meusPontos = 0;
 
 function mostraAtor(){
-    image(imagemAtor, xAtor, yAtor, 25, 30);
+    image(imagemAtor, xAtor, yAtor, 30, 30);
 }
 function movimentaAtor(){
     if (keyIsDown(UP_ARROW)){
-        yAtor -= 2;
+        yAtor -= 4;
     }
     if (keyIsDown(DOWN_ARROW)){
         if (verificaYAtor()){
-         yAtor += 2;
+         yAtor += 4;
         }
     }
     if (keyIsDown(RIGHT_ARROW)){
-        xAtor += 2;
+        xAtor += 4;
     }
     if (keyIsDown(LEFT_ARROW)){
-            xAtor -= 2;
+            xAtor -= 4;
     }
 }
 //    hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
 function verificaColisao(){
     for(let i=0; i<imagemCarros.length; i++){
-        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimento, altura, xAtor, yAtor, 10);
+        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimento, altura, xAtor, yAtor, 13);
         if (colisao){
             voltaPosicaoinicialY();
         if (pontoMaiorQueZero()){
@@ -42,7 +42,7 @@ function incluiPontos(){
     textAlign(CENTER);
     textSize(25);
     fill(color(255, 240, 60));
-    text(meusPontos, width / 5, 27)
+    text(meusPontos, 280, 27)
 }
 function marcaPonto(){
     if(yAtor<10){
